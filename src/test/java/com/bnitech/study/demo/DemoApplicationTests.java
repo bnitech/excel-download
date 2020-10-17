2ee7355af01167be62ef0ba146d158bea738b761
bnitech <bnitech88@gmail.com>
2020-10-17 19:55:47 +0900

    Initial commit

---
+++ src/test/java/com/bnitech/study/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnitech.study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
